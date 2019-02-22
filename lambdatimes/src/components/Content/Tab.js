478cf307f4094813727521f3d4107eb0b3239093
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-22 13:13:54 -0500

    Passed all props through for proper flow of data from <Content /> component to <Tab /> component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Tab = props => {
+  console.log("I'm from the <Tab /> component.")
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={`tab${props.tab === props.selectedTab ? " active-tab" : ""}`}
+      selectTabHandler={props.selectTabHandler(props.tab)}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
     >
       {props.tab.toUpperCase()}
     </div>
