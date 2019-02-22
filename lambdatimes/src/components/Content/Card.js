c0d22b39c8d4939a0118ca5fe611d63d3a4b1c02
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-22 18:06:19 -0500

    Used prop-types to type-check for all props being passed into Card.js

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
   console.log(props.card);
@@ -14,6 +15,15 @@ const Card = props => {
     </div>
   );
 };
+
+Card.propTypes = {
+  card: PropTypes.shape({ 
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+  })
+}
 
 // Make sure to include PropTypes.
 
