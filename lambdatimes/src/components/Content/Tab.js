8ee95c11504b229fb09067c381e3005e56970f9c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-22 13:29:22 -0500

    Got the onClick() handler in Tab.js to function properly

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -8,7 +8,7 @@ const Tab = props => {
   return (
     <div
       className={`tab${props.tab === props.selectedTab ? " active-tab" : ""}`}
-      selectTabHandler={props.selectTabHandler(props.tab)}
+      onClick={() => props.selectTabHandler(props.tab)}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
     >
